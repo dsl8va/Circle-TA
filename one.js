9db0fc1f41eafc810235c0d269243221c7569411
Daniel Lee <dsl8va@gmail.com>
2021-09-28 13:39:04 -0400

    first commit

---
+++ one.js
@@ -0,0 +1,7 @@
+// Create a function that logs “Hello World” to the console every 10 seconds.
+
+function logger() {
+    console.log('Hello World');
+}
+
+window.setInterval(logger, 10000);
---
+++ two.js
@@ -0,0 +1,26 @@
+/* 
+Create a function that sets a cookie with an initial value of 0 on the page. Each time on
+the page loads, add 1 to the cookie’s value and log it to the console. For example, the
+value of the cookie would be 1 on the first page load, 2 on the second load, etc. Extra
+Credit: On the page, create a button that resets the cookie’s value to 0 when clicked.
+*/
+
+function setCookie() {
+    let cookie = document.cookie;
+    console.log(`Cookie count is ${cookie}`);
+    let num = parseInt(cookie);
+    if (typeof(num) == 'number') {
+        let newNum = num + 1;
+        document.cookie = newNum.toString();
+    } else {
+        document.cookie = '0';
+    }
+    return;
+}
+
+function reset() {
+    document.cookie = '0';
+    console.log(`Cookie reset to ${document.cookie}`);
+}
+
+setCookie();
